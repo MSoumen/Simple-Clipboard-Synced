d8753586d86be0306ae38632aeedf5316414c5d0
MSoumen <soumenmahata05@gmail.com>
2025-05-05 07:29:18 +0530

    dropdown ongoing, RESTful communication implemented.

--- TCS_BANCS/src/main/java/com/bancs/repository/ParamInputRepository.java
+++ TCS_BANCS/src/main/java/com/bancs/repository/ParamInputRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.bancs.model.ParamInput;
 
+@Repository
 public interface ParamInputRepository extends JpaRepository<ParamInput, Long>{
 	@Override
 	@Query(value="select * from T_Param_Input", nativeQuery = true)
